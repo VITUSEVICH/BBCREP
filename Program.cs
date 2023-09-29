1383bbaf50c5944b584edf5d1994680d16f86902
Витусевич Владислав <vladzero5559@gmail.com>
2023-09-29 13:09:08 +0300

    Fixing

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ namespace BBC
         {
             Console.WriteLine("Введите вашу фамилию:");
             string surname = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст:");
+            int age =int.Parse (Console.ReadLine());
             Console.WriteLine("Ваша фамилия:" + surname);
+            Console.WriteLine("Ваш возраст:" + age);
             Console.ReadLine();
 
         }
