2ff555db3e4e7d3f2d375bc62c9b94a917ddb0a6
Витусевич Владислав <vladzero5559@gmail.com>
2023-09-29 12:44:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BBC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
